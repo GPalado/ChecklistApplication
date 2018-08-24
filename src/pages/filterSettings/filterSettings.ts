515bab7d7b826607d6cf526a15f28886a27609d2
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-08-24 14:30:38 +1200

    Made labels show in filter settings

--- src/pages/filterSettings/filterSettings.ts
+++ src/pages/filterSettings/filterSettings.ts
@@ -1,28 +1,76 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { NewLabelPage } from '../newLabel/newLabel';
+import { AngularFireDatabase, AngularFireList } from '../../../node_modules/angularfire2/database';
+import { FormBuilder, FormControl, FormArray } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'page-filterSettings',
   templateUrl: 'filterSettings.html'
 })
 export class FilterSettingsPage {
-  filter = {}
+  labels;
+  filters: AngularFireList<any>;
+  formControl = this.formBuilder.group({
+    viewAll: false,
+    labels: new FormArray([])
+  });
+  filtersObj;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { 
-    // todo filters in the database
+  constructor(public navCtrl: NavController, public navParams: NavParams, public database: AngularFireDatabase, public formBuilder: FormBuilder) { 
+    this.filters = database.list('/filters/');
+    this.filtersObj = this.database.object('/filters/');
+    database.object('/labels').valueChanges().subscribe(data => {
+      if(data) {
+        console.log('label data ', data); // object of kv pairs
+        this.labels = Object.entries(data).map(([key, value]) => ({key,value})); // array of objects
+        if(this.labels) {
+          const controls = this.labels.map(c => new FormControl(false));
+          for(let i in controls){
+            let dataKey = Object.keys(data)[i];
+            let isChecked: Boolean = data[dataKey]['checked'];
+            if(isChecked){
+              controls[i].value = true;
+            }
+          }
+          let isAll: boolean = this.filtersObj['all'];
+          this.formControl = this.formBuilder.group({
+            viewAll: isAll,
+            labels: new FormArray(controls)
+          });
+        }
+      } else {
+        console.log('No labels');
+      }
+    });
   }
   
   addNewLabel() {
     this.navCtrl.push(NewLabelPage);
   }
 
-  updateFilters() {
-    this.navCtrl.pop();
+  save() {
+    const selected = this.formControl.value.labels
+      .map((v, i) => v ? this.labels[i] : null)
+      .filter(v => v !== null);
+
+    console.log('Selected ', selected);
+    // take back to home
+    this.navCtrl.goToRoot({});
   }
 
-  updateAll() {
-    // todo either activate or deactivate all other filters
+  allChanged() {
+    let isAll = this.formControl.get('viewAll').value;
+    this.filtersObj.update({
+      all: isAll
+    });
+    for(let i in this.formControl.value.labels) {
+      if(isAll) {
+       this.formControl.value.labels[i].disable();
+      } else {
+        this.formControl.value.labels[i].enable();
+      }
+    }
   }
 
 }
